fc1137cc0294cdb6fa46b802ddf20aed528cf05a
Zohaib Malik <57034343+zohaibmalikie@users.noreply.github.com>
2021-02-04 19:07:38 +0500

    fixes

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -4,34 +4,36 @@ import Logo from '../assets/img/top_logo.png'
  function navbar() {
     return (
         <React.Fragment>
-<div className="pl-3 pr-5 pt-4">
+<div className="pl-2 pr-5 padding-top4">
 <div className="row">
     <div className="col-md-3 p-0 text-center">
-        <img src={Logo} alt="There is logo" className="img-fluid w-75"/>
+        <img src={Logo} alt="There is logo" className="img-fluid w-64"/>
     </div>
-    <div className="col-md-5 d-flex align-self-end p-0">
-<div className="col-md-3 p-0 mb-1 text-center text-uppercase">
-    <NavItems name="Component"/>
+    <div className="col-md-5 d-flex align-self-end text-left p-0">
+<div className="col-md-3 p-0 mb-2 text-center text-uppercase">
+    <NavItems name="Competitions"/>
 </div>
-<div className="col-md-3 p-0 mb-1 text-center text-uppercase">
+<div className="col-md-3 p-0 mb-2 text-center text-uppercase">
 <NavItems name="Winner"/>
 </div>
-<div className="col-md-3 p-0 mb-1 text-center text-uppercase">
+<div className="col-md-2 p-0 mb-2 text-center text-uppercase">
 <NavItems name="About"/>
 </div>
-<div className="col-md-3 p-0 mb-1 text-center text-uppercase ">
+<div className="col-md-3 p-0 mb-2 text-center text-uppercase ">
 <NavItems name="Contact us"/>
 </div>
     </div>
     <div className="col-md-4 d-flex align-self-end p-0 justify-content-end mb-1">
-        <div className="col-md-3">
-        <button type="button" class="btn btn-danger font border-radius  ">Login</button>
+        <div className="col-md-8">
+            <div className="row justify-content-end">
+
+        <button type="button" class="btn bg-red-color font border-radius  ">Login</button>
+        <button type="button" class="btn bg-red-color font border-radius  ml-3">Signup</button>
         </div>
-        <div className="col-md-3">
-        <button type="button" class="btn btn-danger font border-radius  ml-3">Signup</button>
-        </div>
-        <div className="col-md-3 text-right h1 p-0 m-0">
-           <i class="fa fa-shopping-cart text-danger m-0" aria-hidden="true"></i>
+            </div>
+        <div className="col-md-4  h1 p-0 m-0 d-flex align-self-start justify-content-center">
+            <p className="zero ">O</p>
+           <i class="fa fa-shopping-cart red-color m-0" aria-hidden="true"></i>
         </div>
     </div>
 </div>
@@ -43,8 +45,8 @@ import Logo from '../assets/img/top_logo.png'
 }
 const NavItems =({name})=>{
     // const {name} = props;
-return (<p className="font"> {name}</p>)}
-
+return (<p className="font m-0"> {name}</p>)}
+ 
 
 export default navbar;
 export {NavItems};
